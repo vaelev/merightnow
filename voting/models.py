bc7352a70e5cb2717fe76525eff7d34eedd7ed0e
vaelev <johanncasparzahlmann@gmail.com>
2023-12-20 12:54:20 +0100

    Deleted pycache folders from git tracking, changed db to postgres, fixed emoji click bug

--- voting/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Feeling(models.Model):
-    emoji = models.CharField(max_length=10)
-    timestamp = models.DateTimeField(auto_now_add=True)
--- voting/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('handle_emoji_click/', views.handle_emoji_click, name='handle_emoji_click'),
-    path('emoji_percentage/', views.emoji_percentage, name='emoji_percentage'),
-]
